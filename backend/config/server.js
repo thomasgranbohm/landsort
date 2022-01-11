a334fbc19368679404838b1a045031554bbcc509
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-01-11 21:26:57 +0100

    Updated config files

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,4 +1,12 @@
 module.exports = ({ env }) => ({
 	host: env('HOST', '0.0.0.0'),
 	port: env.int('PORT', 1337),
+	url: env('URL', 'http://localhost:1337'),
+	admin: {
+		auth: {
+			secret: env('ADMIN_JWT_SECRET', '960d6049a582837941e25b783f5c6407'),
+		},
+		url: env('ADMIN_URL', 'http://localhost:1337/admin'),
+		autoOpen: false,
+	},
 });
