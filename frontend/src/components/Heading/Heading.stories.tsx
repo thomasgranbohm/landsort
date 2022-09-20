752137615dafe85a3cb3961ab16a27a6e9c13dac
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-09-20 11:12:18 +0200

    Update Heading story

--- frontend/src/components/Heading/Heading.stories.tsx
+++ frontend/src/components/Heading/Heading.stories.tsx
@@ -1,22 +1,22 @@
-  import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-  import Heading from './Heading';
+import Heading from './Heading';
 
-  export default {
-    title: 'Heading',
-    component: Heading,
-    args: {
-      children: 'Lorem ipsum',
-      type: 'h1',
-    },
-    argTypes: {
-      type: {
-        control: 'select',
-        options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'b'],
-      },
-    },
-  } as ComponentMeta<typeof Heading>;
+export default {
+	title: 'Heading',
+	component: Heading,
+	args: {
+		children: 'Lorem ipsum',
+		type: 'h1',
+	},
+	argTypes: {
+		type: {
+			control: 'select',
+			options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'b'],
+		},
+	},
+} as ComponentMeta<typeof Heading>;
 
-  export const Primary: ComponentStory<typeof Heading> = ({ children, type }) => (
-    <Heading type={type}>{children}</Heading>
-  );
+export const Primary: ComponentStory<typeof Heading> = ({ children, type }) => (
+	<Heading type={type}>{children}</Heading>
+);
